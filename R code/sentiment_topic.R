0acea30566db710e6dc1e58765e563aa19c9487f
TistaNath0506 <33410179+TistaNath0506@users.noreply.github.com>
2017-12-07 12:05:16 +0530

    R code

--- R code/sentiment_topic.R
+++ R code/sentiment_topic.R
@@ -1,30 +1,29 @@
-library(ggplot2)
+tw_cln
+
+
+x<- str_extract(tw3, "#\\S+")
+x
+a= na.omit(x)
+#===========================
 library(tidytext)
 library(dplyr)
 library(tm)
 library(plyr)
-library(sentimentr)
-library(syuzhet)
-library(ggplot)
-library(rJava)
-library(wordnet)
-library(stringr)
-library(wordcloud)
-library(RColorBrewer)
 
 tweets<-read.csv(file.choose())
 tweets
 names(tweets)
-tweets=tweets[,1:5]
 
+#tweets_df=ldply(tweets,function(t) t$toDataFrame())
+#tweets_df=data.frame(tweets)
+#twt1=sapply(tweets_df,function(x) x$getText())
 tw=tweets[,3]
 tw
 tw1=gsub("(RT|via)((?:\\b\\w*@\\w+)+)","",tw)
 tw2=gsub("http[^[:blank:]]+","",tw1)
 tw3=gsub("@\\w+","",tw2)
-
-#tw4=gsub("[[:punct:]]","",tw3)
-#tw5=gsub("[^[:alnum:]]","",tw4)
+tw4=gsub("[[:punct:]]","",tw3)
+tw5=gsub("[^[:alnum:]]","",tw4)
 
 tw=tolower(tw)
 tw
@@ -41,18 +40,20 @@ tw_cln[i]=removeWords(as.character(tw[i]), cl_list)
 
 tw_cln
 
-
+library(RColorBrewer)
 pal=brewer.pal(8,"Dark2")
-
+library(wordcloud)
 wordcloud(tw_cln,min.freq=1,max.words=1000,width=1000,height=10200,random.order=F,color=pal)
 
+library(sentimentr)
+library(syuzhet)
 
 sentiment=get_nrc_sentiment(tw_cln)
 sentiment_scores=data.frame(colSums(sentiment[,]))
 names(sentiment_scores)="scores"
 sentiment_scores=cbind("sentiment"=rownames(sentiment_scores),sentiment_scores)
 rownames(sentiment_scores)=NULL
-
+library(ggplot2)
 
 ggplot(data=sentiment_scores,aes(x=sentiment,y=scores))+geom_bar(aes(fill=sentiment),stat="identity")+theme(legend.position="none")+
         xlab("sentiment")+ylab("scores")+ggtitle("Total sentimental scores")
@@ -75,7 +76,6 @@ j=i+1
 w=w+1
 }
 }
-
 word
 tabw=table(word)
 tabww=data_frame(word = names(tabw), count = as.numeric(tabw)) 
@@ -89,13 +89,16 @@ get_sentiment_dictionary()
 get_sentiment_dictionary('bing')
 get_sentiment_dictionary('afinn')
 get_sentiment_dictionary('nrc')
-
+library(rJava)
+library(wordnet)
 
 write.csv(wordf, file = "word.csv")
 
 
-#extracting hash words
+#==============================
 
+#extracting hash words
+library(stringr)
 tw3
 x<- str_extract(tw3, "#\\S+")
 x
